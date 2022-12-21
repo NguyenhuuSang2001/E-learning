85ccc4a109508d935f9dd5863b618f05e00e8430
Nguyễn Hữu Sang <nguyenhuusang@Host.local>
2022-12-21 14:59:54 +0700

    update base code

--- BackEnd/src/resources/views/components/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer style="background-color: #3a4bcd;">
-    <hr>
-    <div class="container">
-        <p class="text-center" style="padding: 15px 0px; color: white;">Copyright &copy; {{ date('Y') }} by
-            Quiz</p>
-    </div>
-</footer>
